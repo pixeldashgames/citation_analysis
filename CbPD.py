3b6bf0f9d498c4c5f5b86421203a0f2bd6e4609d
Campione <80720402+alfrecampione@users.noreply.github.com>
2024-02-11 18:47:21 -0500

    Comments added

--- CbPD.py
+++ CbPD.py
@@ -1,41 +1,55 @@
+# Importing necessary libraries
 import spacy
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
 
+# Loading the English language model from spaCy
 nlp = spacy.load('en_core_web_lm')
 
 
+# Function to clean the input text
 def clean_string(text):
+    # Creating a Doc object
     doc = nlp(text)
+    # Lemmatizing the words, removing stop words and joining them back into a string
     cleaned_text = " ".join(token.lemma_ for token in doc if not token.is_stop)
     return cleaned_text
 
 
+# Function to extract citations from the text
 def extract_citations(text):
+    # Creating a Doc object
     doc = nlp(text)
-    # searching for titles of works and organizations
+    # Extracting entities that are either works of art or organizations
     citations = [ent.text for ent in doc.ents if ent.label_ in ['WORK_OF_ART', 'ORG']]
-
-    # Post-processing: remove citations that are too short
+    # Removing citations that are too short
     citations = [citation for citation in citations if len(citation.split()) > 2]
     return citations
 
 
+# Function to calculate the similarity between two texts
 def calculate_similarity(text1, text2):
-    tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate([text1, text2])]
+    # Preparing the data for the Doc2Vec model
+    tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in
+                   enumerate([text1, text2])]
+    # Training the Doc2Vec model
     model = Doc2Vec(tagged_data, vector_size=20, window=2, min_count=1, epochs=100)
+    # Inferring vectors for the input texts
     vector1 = model.infer_vector(word_tokenize(text1.lower()))
     vector2 = model.infer_vector(word_tokenize(text2.lower()))
+    # Calculating the similarity between the inferred vectors
     return model.docvecs.similarity_unseen_docs(model, vector1, vector2)
 
 
+# Function to detect plagiarism between two texts
 def detect_plagiarism(text1, text2):
+    # Cleaning the input texts
     cleaned_text1 = clean_string(text1)
     cleaned_text2 = clean_string(text2)
-
+    # Extracting citations from the cleaned texts
     citations1 = extract_citations(cleaned_text1)
     citations2 = extract_citations(cleaned_text2)
-
+    # Calculating the similarity between the citations
     citation_similarity = calculate_similarity(' '.join(citations1), ' '.join(citations2))
-
+    # Returning the similarity score
     return citation_similarity
